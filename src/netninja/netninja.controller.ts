e07c2f45179cd578a61561b60a5fd9583594ce2f
mastewal <mastewaldegu0@gmail.com>
2024-08-21 16:26:38 -0300

    create nest controller

--- src/netninja/netninja.controller.ts
+++ src/netninja/netninja.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('netninja')
-export class NetninjaController {}
+export class NetninjaController {
+
+    @Get()
+    getNetNinja(){
+        return []
+    
+    }
+    @Get(":id")
+    getOneNetNinja(@Param('id') id:String){
+        return {id}
+
+    }
+    @Post()
+    createNetNinja(){
+
+    }
+    @Put(':id')
+    updateNetNinja(@Param("id") id:string){
+
+    }
+    @Delete(":id")
+    deleteNetNinja(@Param('id') id:string){
+
+    }
+
+
+
+}
+
